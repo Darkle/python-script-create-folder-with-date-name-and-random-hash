9c478c3b15edb772802dc17d2af886cf0d8028c8
Coop Coding <coop.coding@gmail.com>
2018-03-08 08:27:41 +1100

    update

--- devnotes.md
+++ devnotes.md
@@ -1,3 +1,3 @@
-pipenv install coconut[watch,jobs,mypy,cPyparsing] prospector invoke watchgod
+pipenv install coconut[watch,jobs,mypy,cPyparsing] prospector[with_vulture,with_pyroma] invoke watchgod
 
 
--- tasks.py
+++ tasks.py
@@ -6,20 +6,35 @@ from watchgod import run_process
 @task
 def cocowatch(context):
     print("cocowatch running")
-    os.system("coconut ./ --watch --standalone --strict --target sys")
+    os.system("coconut ./ --watch --package --strict --target sys")
 
 def runProspector():
-    os.system('prospector app.py')
+    # os.system('prospector app.py --with-tool pyroma --with-tool vulture')
+    """
+    Vulture doesn't seem to be working properly. Get an error of:
+        Line: None
+        vulture: failure / Tool vulture failed to run (exception was raised)
+    https://github.com/landscapeio/prospector/issues?utf8=%E2%9C%93&q=vulture
+    """
+    os.system('prospector app.py --with-tool pyroma')
 
 @task
 def lintwatch(context):
     print("lintwatch running")
     run_process('./', runProspector)
 
-def runProgram():
+def runApp():
     os.system('python app.py')
 
 @task
-def programwatch(context):
-    print("programwatch running")
-    run_process('./', runProgram)
+def appwatch(context):
+    print("appwatch running")
+    run_process('./', runApp)
+
+def runMypy():
+    os.system('mypy app.py --follow-imports=silent')
+
+@task
+def mypyWatch(context):
+    print("mypyWatch running")
+    run_process('./', runMypy)
